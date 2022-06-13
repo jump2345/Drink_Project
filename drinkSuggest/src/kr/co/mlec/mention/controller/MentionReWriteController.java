1825d78e6314b6b88c62aa874920b08ede00240e
sh <lop9036@naver.com>
2022-06-13 18:10:59 +0900

    리팩토링1. 답글,대댓글 기능추가

---
+++ drinkSuggest/src/kr/co/mlec/mention/controller/MentionReWriteController.java
@@ -0,0 +1,77 @@
+package kr.co.mlec.mention.controller;
+
+import kr.co.mlec.controller.Controller;
+import kr.co.mlec.mention.dao.MentionDAO;
+import kr.co.mlec.mention.service.MentionService;
+import kr.co.mlec.mention.vo.MentionVO;
+
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
+public class MentionReWriteController implements Controller {
+
+    @Override
+    public String handleRequest(HttpServletRequest request, HttpServletResponse response) throws Exception {
+
+        request.setCharacterEncoding("utf-8");
+
+
+        MentionDAO dao = MentionDAO.getInstance();
+
+        // 파리미터 값을 가져온다.
+        int commentNo = Integer.parseInt(request.getParameter("commentNo"));
+        String mentionID = request.getParameter("mentionID");
+        String mentionContent = request.getParameter("mentionContent");
+        int mention = Integer.parseInt(request.getParameter("rementionNo"));
+
+        System.out.println("멘션 컨트롤러" + commentNo);
+        System.out.println("멘션 컨트롤러" + mentionID);
+        System.out.println("멘션 컨트롤러" + mentionContent);
+        System.out.println("멘션 컨트롤러" + mention);
+
+
+        MentionVO mentionVO = new MentionVO();
+        mentionVO.setMentionNo(dao.selectMentionSeq());    // 댓글 번호는 시퀀스값으로
+        mentionVO.setCommentNo(commentNo);
+        mentionVO.setMentionId(mentionID);
+        mentionVO.setMentionContent(mentionContent);
+        mentionVO.setMentionParent(mention);
+
+
+        System.out.println("멘션 컨트롤러" + mentionVO);
+
+
+
+        // System.out.println("ㅠㅠ"+commentNo);
+        //?....
+        /*
+         * int comNo =Integer.parseInt(request.getParameter("comNo")); BoardVO board =
+         * new BoardVO(); board.setComNo(comNo);
+         */
+
+	        /*
+	        int comNo = Integer.parseInt(request.getParameter("comNo"));
+	        BoardVO board = new BoardVO();
+			board.setComNo(comNo);*/
+	        /*
+	        boolean result = dao.insertMention(mentionVO);
+
+	        if(result){
+	            response.setContentType("text/html;charset=euc-kr");
+	            PrintWriter out = response.getWriter();
+	            out.println("1");
+	            out.close();
+	        }
+
+	        */
+
+        MentionService service = new MentionService();
+        service.insertMention(mentionVO);
+
+        //?...
+//	        return "redirect:/board/detail.do?no=" + commentNo;
+        return "/jsp/board/replyResult.jsp";
+
+
+    }
+}
